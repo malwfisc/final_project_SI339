dc44526312fd87ba025441e5a461fef01b44fd02
malwfisc <143574553+malwfisc@users.noreply.github.com>
2023-12-04 03:23:50 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,9 +3,9 @@ function toggleFavorite(star) {
 }
 
 function mDown(obj) {
-    obj.src = "../../images/mel_2.jpg";
+    obj.src = "final_project_SI339/images/mel_2.jpg";
   }
   
 function mUp(obj) {
-    obj.src = "./images/mel.jpg";
+    obj.src = "../final_project_SI339/images/mel.jpg";
   }
