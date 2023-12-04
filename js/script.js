039f6a5df8f4da9f1b2e27f5a8e6f9815ab0a5d2
malwfisc <143574553+malwfisc@users.noreply.github.com>
2023-12-04 04:37:51 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,9 +2,11 @@ function toggleFavorite(star) {
     star.classList.toggle("favorite");
 }
 
-function mDown(obj) {
-    obj.src = "../final_project_SI339/images/mel_2.jpg";
-  }
+// function mDown(obj) {
+//     obj.src = "../final_project_SI339/images/mel_2.jpg";
+//   }
+
+//   "../images/mel_2.jpg"; "../../images/mel_2.jpg"; "images/mel.jpg";
   
 function mUp(obj) {
     obj.src = "../final_project_SI339/images/mel_2.jpg";
